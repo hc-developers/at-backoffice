d04495223f84a5f2328bfa8de419b0b49cdcf4fe
felipe <felipe@hcdesenvolvimento.com.br>
2023-07-24 20:01:04 -0300

    feat/ATCM-127 comum components

---
+++ backoffice/src/components/Container/style.ts
@@ -0,0 +1,14 @@
+import { Box,  styled } from "@mui/material";
+
+export const DivContainer = styled(Box)`
+display: flex;
+align-items: center;
+flex-direction: column;
+gap: 30px;
+max-width: 1449px;
+width: 80%;
+border-radius: 16px;
+border: 1px solid red;
+margin-left: 160px;
+margin-top: 20px;
+`
